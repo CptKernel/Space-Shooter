75c36fa91a36e0a1753906a4672a577f4809e433
Ethan Cunningham <ethancunninghamcs@gmail.com>
2020-02-06 17:27:24 -0600

    feat: Added laser spawn on space hit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,13 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
+
+        // If i hit the space key
+        // spawn game object
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }
     }
 
     void CalculateMovement()
