c0baed7a4d22ffadda5f059c825d2ac54b1886fd
Ethan Cunningham <ethancunninghamcs@gmail.com>
2020-02-06 16:49:45 -0600

    fix: code cleanup, Made functions for player movement and optimized it

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,28 +16,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
         transform.Translate(direction * _speed * Time.deltaTime);
 
-        // If player position on the y is greater then 0 
-        // y postion = 0
-        
-        if (transform.position.y >= 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        }
-        else if (transform.position.y <= -4.91f)
-        {
-            transform.position = new Vector3(transform.position.x, -4.91f, 0);
-        }
+        // Clamp is useful for setting boundries like this. 
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4.91f, 0), 0);
 
-
-        // if player position on the x axis is greater then 10.24
-        // wrap player back to -11.26
-        // else if player is less then -11.26 wrap back to 10.24
         if (transform.position.x >= 10.24f)
         {
             transform.position = new Vector3(-11.26f, transform.position.y, 0);
